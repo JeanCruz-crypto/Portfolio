b6e6a2c56ad631b3363d1568000d924d7b8ff3ef
Kevin Cruz <cruzk1257@gmail.com>
2025-09-22 13:51:18 -0500

    fix array into object

--- logicAndFunctionality/formulary.js
+++ logicAndFunctionality/formulary.js
@@ -4,11 +4,11 @@ const biggerInputForm = document.querySelector(".biggerInputForm");
 const buttonForForms = document.querySelector(".buttonForForms");
 
 buttonForForms.addEventListener('click', () => {
-  const fullDataObject = [
-    {formName: contactInfoFormName.value},
-    {formEmail: contactInfoFormEmail.value},
-    {messageForm: biggerInputForm.value}
-  ];
+  const fullDataObject = {
+    formName: contactInfoFormName.value,
+    formEmail: contactInfoFormEmail.value,
+    messageForm: biggerInputForm.value
+  };
   
   console.log(fullDataObject);
 });

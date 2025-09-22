d0734a2e53cf9794d12322e2061f864bf543e8ea
JeanCruz-crypto <andercruz2504@gmail.com>
2025-09-22 10:46:22 -0500

    y

---
+++ logicAndFunctionality/formulary.js
@@ -0,0 +1,14 @@
+const contactInfoFormName = document.querySelector('input[type="Name"]');
+const contactInfoFormEmail = document.querySelector('input[type="Email"]');
+const biggerInputForm = document.querySelector(".biggerInputForm");
+const buttonForForms = document.querySelector(".buttonForForms");
+
+buttonForForms.addEventListener('click', () => {
+  const fullDataObject = [
+    {formName: contactInfoFormName.value},
+    {formEmail: contactInfoFormEmail.value},
+    {messageForm: biggerInputForm.value}
+  ];
+  
+  console.log(fullDataObject);
+});
